8437ca9a7b8eec91981d1588c402a07b7a996b0a
Yuyang Huang <yuyangh@usc.edu>
2019-07-25 15:49:39 +0800

    update hashtable to array

--- multi_queue_epoll_server.cpp
+++ multi_queue_epoll_server.cpp
@@ -26,13 +26,20 @@
 #include "mullti_thread_worker.h"
 
 using namespace std;
-#define NUM_WORKERS            32
+
+/*-------------------------------------------------
+ * global variables
+ * -------------------------------------------------
+ */
+#define NUM_WORKERS            20
 #define ECHO_SERVER_PORT        6000
 #define NUM_FD                  1200
 #define LISTEN_BACKLOG          16
 #define MAX_EPOLL_EVENT_COUNT   (NUM_FD>>2)
 #define EPOLL_WAIT_TIMEOUT      0
 
+// todo profiling purpose
+static unsigned long JobCountLimit=2001000L;
 
 static std::vector<MultiThreadWorker> works(NUM_WORKERS);
 static int epoll_fd, server_fd;
@@ -41,15 +48,13 @@ static volatile bool running = true;
 static struct epoll_event event;
 
 static unsigned long long JobCount = 0;
-static unordered_map<int, Package> ClientInfoMap;
-static unordered_map<int, ProcessStatus> FDProcessStatus;
+static Package ClientInfoArr[NUM_FD];
+static ProcessStatus FDProcessStatusArr[NUM_FD];
 
-// static Package ClientInfoArr[NUM_FD];
-// static ProcessStatus FDProcessStatusArr[NUM_FD];
-// todo may optimize unordered_map to array to accelerate
-// static ProcessStatus FDProcessStatusArr[NUM_FD];
-
-
+/*-------------------------------------------------
+ * function prototypes
+ * -------------------------------------------------
+ */
 void process(struct epoll_event *epollEvent);
 
 void ProgramTerminated();
@@ -119,7 +124,7 @@ void ReceivePackage(struct epoll_event *epollEvent) {
 	
 	int epollEventFD = epollEvent->data.fd;
 	// in receiving, so not in ready stage
-	assert(!FDProcessStatus[epollEventFD].ready_to_receive_);
+	assert(!FDProcessStatusArr[epollEventFD].ready_to_receive_);
 	
 	// receive the Package from the client
 	Package buffer;
@@ -138,8 +143,8 @@ void ReceivePackage(struct epoll_event *epollEvent) {
 		close(epollEventFD);
 		
 		// update ready_to_send_ and ready_to_receive_ status
-		FDProcessStatus[epollEventFD].ready_to_send_ = true;
-		FDProcessStatus[epollEventFD].ready_to_receive_ = true;
+		FDProcessStatusArr[epollEventFD].ready_to_send_ = true;
+		FDProcessStatusArr[epollEventFD].ready_to_receive_ = true;
 		return;
 	} else {
 		memcpy(&buffer, recvPackage, sizeof(Package));
@@ -157,7 +162,8 @@ void ReceivePackage(struct epoll_event *epollEvent) {
 #endif
 	
 	// store the received information
-	ClientInfoMap[epollEventFD] = (buffer);
+	ClientInfoArr[epollEventFD] = (buffer);
+	
 
 #ifdef DEBUG_OUTPUT
 	printf("Store clientInfo: fd: %d, value:%d \n", epollEventFD, buffer.value.id);
@@ -166,7 +172,7 @@ void ReceivePackage(struct epoll_event *epollEvent) {
 	epoll_ctl(epoll_fd, EPOLL_CTL_MOD, epollEventFD, epollEvent);
 	
 	// receive done, able to send
-	FDProcessStatus[epollEventFD].ready_to_send_ = true;
+	FDProcessStatusArr[epollEventFD].ready_to_send_ = true;
 }
 
 // current version
@@ -174,15 +180,15 @@ void SendResult(struct epoll_event *epollEvent) {
 	
 	int epollEventFD = epollEvent->data.fd;
 	// in sending stage, so this fd is no longer in ready stage
-	assert(!FDProcessStatus[epollEventFD].ready_to_send_);
+	assert(!FDProcessStatusArr[epollEventFD].ready_to_send_);
 	
 	// send the response
 #ifdef DEBUG_OUTPUT
-	printf("Read ClientInfoMap: fd: %d, value:%d \n", epollEventFD,
-		   ClientInfoMap[epollEventFD].value.id);
+	printf("Read ClientInfoArr: fd: %d, value:%d \n", epollEventFD,
+		   ClientInfoArr[epollEventFD].value.id);
 #endif
 	
-	Result result(ClientInfoMap[epollEventFD].value.id);
+	Result result(ClientInfoArr[epollEventFD].value.id);
 	int sendbytes = send(epollEventFD, (char *) &result, sizeof(Result), 0);
 	if (sendbytes < 0) {
 		perror("send failed.\n");
@@ -202,7 +208,7 @@ void SendResult(struct epoll_event *epollEvent) {
 	epoll_ctl(epoll_fd, EPOLL_CTL_MOD, epollEventFD, epollEvent);
 	
 	// already send, so able to receive
-	FDProcessStatus[epollEventFD].ready_to_receive_ = true;
+	FDProcessStatusArr[epollEventFD].ready_to_receive_ = true;
 }
 
 // receive and send together
@@ -243,9 +249,10 @@ void process(struct epoll_event *epollEvent) {
 void InitializeWorkers(std::vector<std::thread> &workers) {
 	for (int i = 0; i < NUM_WORKERS; ++i) {
 		workers.push_back(std::thread(ProcessJob, i));
-		if (workers[i].joinable()) {
-			workers[i].detach();
-		}
+		workers.back().detach();
+		// if (workers[i].joinable()) {
+		// 	workers[i].detach();
+		// }
 	}
 }
 
@@ -284,8 +291,6 @@ int main(int argc, char *argv[]) {
 	printf("start %s \n", argv[0]);
 	
 	signal(SIGINT, sig_handler);
-	ClientInfoMap.reserve(NUM_FD);
-	FDProcessStatus.reserve(NUM_FD);
 	
 	int i, maxi, server_fd, new_client_fd, sockfd, nfds/*number of fd s*/, portnumber;
 	ssize_t n;
@@ -346,7 +351,6 @@ int main(int argc, char *argv[]) {
 	bind(server_fd, (sockaddr * ) & serveraddr, sizeof(serveraddr));
 	listen(server_fd, MAX_EPOLL_EVENT_COUNT);
 	
-	vector<Package> packages(MAX_EPOLL_EVENT_COUNT);
 	
 	// initializeWorkers thread to work
 	std::vector<std::thread> workers(NUM_WORKERS);
@@ -374,12 +378,6 @@ int main(int argc, char *argv[]) {
 		}
 #endif
 		for (i = 0; i < nfds; ++i) {
-			
-			// todo debug code
-			if (JobCount == 20001000LL) {
-				printf("reach the JobCount\n");
-				exit(0);
-			}
 			
 			Package buffer;
 			//如果新监测到一个SOCKET用户连接到了绑定的SOCKET端口，建立新的连接。
@@ -417,9 +415,9 @@ int main(int argc, char *argv[]) {
 				
 				// todo may optimize to increase speed
 				// register this client fd as neither receiving nor sending
-				FDProcessStatus[(new_client_fd)] = ProcessStatus();
+				FDProcessStatusArr[(new_client_fd)] = ProcessStatus();
 				// create the space 
-				ClientInfoMap[new_client_fd] = Package(-1);
+				ClientInfoArr[new_client_fd] = Package(-1);
 				
 				// epoll_ctl - control interface for an epoll file descriptor
 				//  int epoll_ctl(int epoll_fd, int op, int fd, struct epoll_event *event);
@@ -429,9 +427,9 @@ int main(int argc, char *argv[]) {
 			} else {
 #ifdef PARALLEL_SEND_AND_RECEIVE
 				if ((event_array[i].events & EPOLLIN)) {
-					if (FDProcessStatus[event_array[i].data.fd].ready_to_receive_) {
+					if (FDProcessStatusArr[event_array[i].data.fd].ready_to_receive_) {
 						// go to receiving stage, so no longer in ready stage
-						FDProcessStatus[event_array[i].data.fd].ready_to_receive_ = false;
+						FDProcessStatusArr[event_array[i].data.fd].ready_to_receive_ = false;
 #ifdef DEBUG_OUTPUT
 						printf("fd: %d \t ready_to_receive_ polling\n", event_array[i].data.fd);
 #endif
@@ -440,12 +438,11 @@ int main(int argc, char *argv[]) {
 #ifdef DEBUG_OUTPUT
 					printf("fd: %d \t waiting for ready_to_receive_ condition \n", event_array[i].data.fd);
 #endif
-					continue;
 				} else {
 					if ((event_array[i].events & EPOLLOUT)) {
-						if (FDProcessStatus[event_array[i].data.fd].ready_to_send_) {
+						if (FDProcessStatusArr[event_array[i].data.fd].ready_to_send_) {
 							// go to sending stage, so no longer in ready stage
-							FDProcessStatus[event_array[i].data.fd].ready_to_send_ = false;
+							FDProcessStatusArr[event_array[i].data.fd].ready_to_send_ = false;
 #ifdef DEBUG_OUTPUT
 							printf("fd: %d \t ready_to_send_ polling\n", event_array[i].data.fd);
 #endif
@@ -454,12 +451,14 @@ int main(int argc, char *argv[]) {
 #ifdef DEBUG_OUTPUT
 						printf("fd: %d \t waiting for ready_to_send_ condition \n", event_array[i].data.fd);
 #endif
-						continue;
 					}
-					perror("ERROR, events wrong type\n");
 				}
-				continue;
-				printf("should not RUNNING_FLAG here\n");
+				
+				// todo debug code
+				if (JobCount == JobCountLimit) {
+					printf("reach the JobCount\n");
+					exit(0);
+				}
 #endif
 
 #ifndef PARALLEL_SEND_AND_RECEIVE
@@ -591,8 +590,6 @@ int main(int argc, char *argv[]) {
 #endif
 			}
 		}
-		
-		
 	}
 	atexit(ProgramTerminated);
 	return 0;
